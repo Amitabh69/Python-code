91c10a6fba9fea7a9c0a3f7e94e6c5c4a1281d42
AMITABH SHRIVASTAVA <136047396+Amitabh69@users.noreply.github.com>
2023-09-17 18:17:45 +0530

    Add files via upload

---
+++ Greatest of three number.py
@@ -0,0 +1,10 @@
+num1=float(input("first number is"))
+num2=float(input("second number is "))
+num3=float(input("third number is"))
+
+if num1>num2:
+    print(str(num1) + "is greatest")
+elif num2>num3:
+    print(str(num2) + "is greatest")
+else:
+    print(str(num3) + "is greatest")
