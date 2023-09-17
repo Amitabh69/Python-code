cf5f403f92adc4058f876df1e312b3df997147ad
AMITABH SHRIVASTAVA <136047396+Amitabh69@users.noreply.github.com>
2023-09-17 18:18:05 +0530

    Add files via upload

---
+++ smallest_amoung_four_number.py
@@ -0,0 +1,14 @@
+num1=float(input("first number is"))
+num2=float(input("second number is "))
+num3=float(input("third number is"))
+num4=float(input("fourth number is"))
+
+if num1<num2:
+    print(num1 )
+elif num2<num3:
+    print(num2)
+elif num3<num4:
+    print(num3)
+else:
+    print(num4)
+
