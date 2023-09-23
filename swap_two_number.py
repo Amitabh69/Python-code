ee631b0185101c6c419b0c6d46adcb2fae398925
AMITABH SHRIVASTAVA <136047396+Amitabh69@users.noreply.github.com>
2023-09-23 18:53:10 +0530

    Add files via upload

---
+++ swap_two_number.py
@@ -0,0 +1,11 @@
+first_number=int(input("  first number is-"))
+second_number=int(input(" second number is-"))
+num=0
+
+num=first_number
+first_number=second_number
+second_number=num
+
+print('first_number='+ str(first_number), 'second_number='+str(second_number))
+
+
