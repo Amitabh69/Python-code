8c990a1f5dcd9c558afd321e0ddab1da4fe00e51
AMITABH SHRIVASTAVA <136047396+Amitabh69@users.noreply.github.com>
2023-10-05 16:15:25 +0530

    Add files via upload

---
+++ roundingoff_program.py
@@ -0,0 +1,5 @@
+input=float(input("input by user--"))
+
+result=round(input,3)         # 3 is the place for the required rounded off digit. No parameter will do
+                              # rounding off of first place only.
+print("rounded off value is--  " + str(result))
