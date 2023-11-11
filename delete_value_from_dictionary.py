4ef289add3875eef2be2aa5cd22cccfb9d4f9faa
AMITABH SHRIVASTAVA <136047396+Amitabh69@users.noreply.github.com>
2023-11-11 21:04:04 +0530

    Add files via upload

---
+++ delete_value_from_dictionary.py
@@ -0,0 +1,12 @@
+# by delete function
+score={"varun":54,"joy":67,"arun":97,"sima":54}
+
+del score["varun"]
+print(score)
+
+# by pop function
+
+score={"varun":54,"joy":67,"arun":97,"sima":54}
+
+pop_item= score.pop("arun")
+print(score)
