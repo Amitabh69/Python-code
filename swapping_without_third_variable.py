ac2567891757b1db684d92628212514031111a7b
AMITABH SHRIVASTAVA <136047396+Amitabh69@users.noreply.github.com>
2023-11-16 20:52:00 +0530

    Add files via upload

---
+++ swapping_without_third_variable.py
@@ -0,0 +1,6 @@
+num1=int(input("enter any number for num1--"))
+num2=int(input("enter any other number for num2--"))
+
+num1,num2=num2,num1
+print("value after swapping of num1--",num1)
+print("value after swapping of num2--",num2)
